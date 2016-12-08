d74e3a1ff7ec1737a6a10de488b2302745cc4b00
Ryan Walklin <ryan@testtoast.com>
2016-12-08 21:20:42 +1100

    Initial Commit

---
+++ AtlasGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AtlasGenerator
+//
+//  Created by Ryan Walklin on 8/12/16.
+//  Copyright © 2016 Ryan Walklin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
